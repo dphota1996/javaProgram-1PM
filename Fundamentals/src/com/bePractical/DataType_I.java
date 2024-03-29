59bd75e5f7378503574fb429c607309267e897bf
devip <devip@Victus.mshome.net>
2024-03-29 14:38:57 +0530

    First commit

---
+++ Fundamentals/src/com/bePractical/DataType_I.java
@@ -0,0 +1,25 @@
+package com.bePractical;
+
+public class DataType_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("byte size:"+Byte.SIZE+" bytes");
+		System.out.println("short size:"+Short.SIZE+" bytes");
+		System.out.println("int size:"+Integer.SIZE+" bytes");
+		System.out.println("long size:"+Long.SIZE+" bytes");
+		System.out.println("float size:"+Float.SIZE+" bytes");
+		System.out.println("double size:"+Double.SIZE+" bytes");
+		System.out.println("char size:"+Character.SIZE+" bytes");
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Printing range of datatypes");
+		System.out.println("byte range :"+Byte.MIN_VALUE+" -> " +Byte.MAX_VALUE);
+		System.out.println("byte range :"+Short.MIN_VALUE+" -> " +Short.MAX_VALUE);
+		System.out.println("byte range :"+Integer.MIN_VALUE+" -> " +Integer.MAX_VALUE);
+		System.out.println("byte range :"+Long.MIN_VALUE+" -> " +Long.MAX_VALUE);
+		System.out.println("byte range :"+Float.MIN_VALUE+" -> " +Float.MAX_VALUE);
+		System.out.println("byte range :"+Double.MIN_VALUE+" -> " +Double.MAX_VALUE);
+
+	}
+
+}
---
+++ Fundamentals/src/com/bePractical/Test.java
@@ -0,0 +1,17 @@
+package com.bePractical;
+class Car{
+	int numberOfwheels = 4;
+	String color ="red";
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		System.out.println(c1.color);
+		System.out.println(c1.numberOfwheels);
+		
+
+	}
+
+}
